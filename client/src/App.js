a549b2627a31bc5546dcef48409314dc69fcc47e
ruiz9984-nbjuKP <ruiz9984@icloud.com>
2023-03-05 16:36:54 -0500

    react app

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <>
+    <h1>Final Countdown</h1>
+    </>
+  );
+}
+export default App;
--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
 app.get("/ping",(req,res) => {
     res.send("pong");
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(BUILD_PATH, "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
-})
+}) 
